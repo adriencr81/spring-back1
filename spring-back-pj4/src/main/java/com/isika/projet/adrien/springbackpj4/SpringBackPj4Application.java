9af65ff00493bccd7ece56d0d7b5de22140351b9
adriencr81 <107180086+adriencr81@users.noreply.github.com>
2023-02-07 18:06:34 +0100

    ajout projet

---
+++ spring-back-pj4/src/main/java/com/isika/projet/adrien/springbackpj4/SpringBackPj4Application.java
@@ -0,0 +1,13 @@
+package com.isika.projet.adrien.springbackpj4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackPj4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackPj4Application.class, args);
+	}
+
+}
---
+++ spring-back-pj4/src/test/java/com/isika/projet/adrien/springbackpj4/SpringBackPj4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isika.projet.adrien.springbackpj4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackPj4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
